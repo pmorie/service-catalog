7b874a84a47f72c120ae211751f398bd9552ce9b
Jeff Peeler <jpeeler@redhat.com>
2017-01-16 14:35:15 -0500

    Uncomment TestRoundTripTypes

--- pkg/apis/servicecatalog/serialization_test.go
+++ pkg/apis/servicecatalog/serialization_test.go
@@ -247,23 +247,23 @@ var catalogGroups = map[string]testapi.TestGroup{
 	"servicecatalog": serviceCatalogAPIGroup(),
 }
 
-// func TestRoundTripTypes(t *testing.T) {
-// 	for groupKey, group := range catalogGroups {
-// 		for kind := range group.InternalTypes() {
-// 			t.Logf("working on %v in %v", kind, groupKey)
-// 			if nonRoundTrippableTypes.Has(kind) {
-// 				continue
-// 			}
-// 			// Try a few times, since runTest uses random values.
-// 			for i := 0; i < *fuzzIters; i++ {
-// 				doRoundTripTest(group, kind, t)
-// 				if t.Failed() {
-// 					break
-// 				}
-// 			}
-// 		}
-// 	}
-// }
+func TestRoundTripTypes(t *testing.T) {
+	for groupKey, group := range catalogGroups {
+		for kind := range group.InternalTypes() {
+			t.Logf("working on %v in %v", kind, groupKey)
+			if nonRoundTrippableTypes.Has(kind) {
+				continue
+			}
+			// Try a few times, since runTest uses random values.
+			for i := 0; i < *fuzzIters; i++ {
+				doRoundTripTest(group, kind, t)
+				if t.Failed() {
+					break
+				}
+			}
+		}
+	}
+}
 
 func testEncodePtr(t *testing.T) {
 	broker := &servicecatalog.Broker{
