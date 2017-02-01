3f81180becc36d63a78e4408c63ccc9ec5932c4a
Kent Rancourt <kent@deis.com>
2017-02-01 09:39:38 -0500

    WIP: Add default OSB GUIDs

---
+++ pkg/apis/servicecatalog/v1alpha1/defaults_test.go
@@ -0,0 +1,68 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1_test
+
+import (
+	"reflect"
+	"testing"
+
+	versioned "github.com/kubernetes-incubator/service-catalog/pkg/apis/servicecatalog/v1alpha1"
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/runtime"
+)
+
+func roundTrip(t *testing.T, obj runtime.Object) runtime.Object {
+	// TODO: I think this next line is WRONG
+	codec := api.Codecs.LegacyCodec(versioned.SchemeGroupVersion)
+	data, err := runtime.Encode(codec, obj)
+	if err != nil {
+		t.Errorf("%v\n %#v", err, obj)
+		return nil
+	}
+	obj2, err := runtime.Decode(codec, data)
+	if err != nil {
+		t.Errorf("%v\nData: %s\nSource: %#v", err, string(data), obj)
+		return nil
+	}
+	obj3 := reflect.New(reflect.TypeOf(obj).Elem()).Interface().(runtime.Object)
+	err = api.Scheme.Convert(obj2, obj3, nil)
+	if err != nil {
+		t.Errorf("%v\nSource: %#v", err, obj2)
+		return nil
+	}
+	return obj3
+}
+
+func TestSetDefaultInstance(t *testing.T) {
+	i := &versioned.Instance{}
+	obj2 := roundTrip(t, runtime.Object(i))
+	i2 := obj2.(*versioned.Instance)
+
+	if i2.Spec.OSBGUID == "" {
+		t.Error("Expected a default OSBGUID, but got none")
+	}
+}
+
+func TestSetDefaultBinding(t *testing.T) {
+	b := &versioned.Binding{}
+	obj2 := roundTrip(t, runtime.Object(b))
+	b2 := obj2.(*versioned.Binding)
+
+	if b2.Spec.OSBGUID == "" {
+		t.Error("Expected a default OSBGUID, but got none")
+	}
+}
