5d6de4ef071148cce4fcaae558bd2f340183ad94
Paul Morie <pmorie@redhat.com>
2017-04-11 15:07:21 -0400

    WIP: hack hack hack

--- pkg/summit/controller.go
+++ pkg/summit/controller.go
@@ -17,13 +17,14 @@ limitations under the License.
 package summit
 
 import (
-	"fmt"
-
 	"github.com/golang/glog"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	runtimeutil "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/pkg/api"
-	"k8s.io/client-go/pkg/api/v1"
+	"k8s.io/client-go/tools/cache"
+
+	kapi "k8s.io/kubernetes/pkg/api"
 
 	"github.com/kubernetes-incubator/service-catalog/pkg/apis/servicecatalog/v1alpha1"
 	servicecatalogclientset "github.com/kubernetes-incubator/service-catalog/pkg/client/clientset_generated/clientset/typed/servicecatalog/v1alpha1"
@@ -35,7 +36,7 @@ import (
 func NewController(
 	kubeClient kubernetes.Interface,
 	serviceCatalogClient servicecatalogclientset.ServicecatalogV1alpha1Interface,
-	osClient osClient.Interface,
+	osClient osclient.Interface,
 	bindingInformer informers.BindingInformer,
 ) (Controller, error) {
 	controller := &controller{
@@ -44,7 +45,7 @@ func NewController(
 		osClient:             osClient,
 	}
 
-	bindingInformer.Informer().AddEventHandler(cache.ResourceEventHandleFuncs{
+	bindingInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc:    controller.bindingAdd,
 		UpdateFunc: controller.bindingUpdate,
 		DeleteFunc: controller.bindingDelete,
@@ -63,6 +64,15 @@ type controller struct {
 	kubeClient           kubernetes.Interface
 	serviceCatalogClient servicecatalogclientset.ServicecatalogV1alpha1Interface
 	osClient             osclient.Interface
+}
+
+// Run runs the controller until the given stop channel can be read from.
+func (c *controller) Run(stopCh <-chan struct{}) {
+	defer runtimeutil.HandleCrash()
+	glog.Info("Starting service-catalog controller")
+
+	<-stopCh
+	glog.Info("Shutting down service-catalog controller")
 }
 
 func (c *controller) bindingAdd(obj interface{}) {
@@ -85,7 +95,7 @@ func (c *controller) reconcileBinding(binding *v1alpha1.Binding) {
 		return
 	}
 
-	if binding.Status.Conditions[0].Type != v1alpha1.BrokerConditionReady {
+	if binding.Status.Conditions[0].Type != v1alpha1.BindingConditionReady {
 		return
 	}
 
@@ -96,7 +106,7 @@ func (c *controller) reconcileBinding(binding *v1alpha1.Binding) {
 	ns := binding.Namespace
 	secretName := binding.Spec.SecretName
 
-	secret, err := c.kubeClient.Core().Secrets(ns).Get(secretName)
+	secret, err := c.kubeClient.Core().Secrets(ns).Get(secretName, metav1.GetOptions{})
 	if err != nil {
 		glog.Errorf("Error getting secret %v/%v", ns, secretName)
 	}
@@ -106,7 +116,7 @@ func (c *controller) reconcileBinding(binding *v1alpha1.Binding) {
 		glog.Errorf("Error getting deployments for namespace %v", ns)
 	}
 
-	for _, dc := range deploymentConfigs {
+	for _, dc := range deploymentConfigs.Items {
 		_, ok := dc.Annotations[specialAnnotationKey]
 		if ok {
 			// we already did this one, next
@@ -114,13 +124,15 @@ func (c *controller) reconcileBinding(binding *v1alpha1.Binding) {
 		}
 
 		for key := range secret.Data {
-			env := v1.EnvVar{
+			env := kapi.EnvVar{
 				Name: key,
-				ValueFrom: &v1.SecretKeyRef{
-					LocalObjectReference: v1.LocalObjectReference{
-						Name: secretName,
+				ValueFrom: &kapi.EnvVarSource{
+					SecretKeyRef: &kapi.SecretKeySelector{
+						LocalObjectReference: kapi.LocalObjectReference{
+							Name: secretName,
+						},
+						Key: key,
 					},
-					Key: key,
 				},
 			}
 
@@ -132,7 +144,7 @@ func (c *controller) reconcileBinding(binding *v1alpha1.Binding) {
 
 		dc.Annotations[specialAnnotationKey] = "set"
 
-		_, err := c.osClient.DeploymentConfigs(ns).Update(dc)
+		_, err := c.osClient.DeploymentConfigs(ns).Update(&dc)
 		if err != nil {
 			glog.Errorf("Error updating deploymentConfig %v/%v", ns, dc.Name)
 		}
